76bca42cbe2eb4ec5b1d14471874557fc9196402
Santiago Duque <santimanga@gmail.com>
2018-11-02 14:39:04 -0500

    Commit inicial generación de la primera aproximación al integrador tipo RK4 y archivo de prueba

---
+++ integrator.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 22:22:14 2018
+
+@author: Santiago
+"""
+from numpy import linspace, array
+
+class RK4:   
+    #def solve(self,f,y0,x0,xf,n,param = None):
+    def solve(f,y0,x0,xf,n,param = None):
+       """
+       @brief     Soliciona un sistema de EDO por el método de Runge-Kutta 4.
+                  Se basa el la función scipy.integrate.odeint para tomar los 
+                  parámetros de entrada.
+       @param f   f(y0,x0,...) Función que calcula la derivada de y en x0.
+       @param y0  Valores iniciales (puede ser un vector).
+       @param x0  Valor inicial de la variable independiente.
+       @param xf  Valor fínal de la variable independiente.
+       @param n   Número de puntos en los que se va a calcular la solución
+       @param param   (Opcional) Lista con los parámetros adicionales que la 
+                      función f necesita.       
+       """
+       #TODO: validar el tipo y el número de n
+       x = linspace(x0,xf,n)       
+       h = x[1] - x[0]
+       res = [y0]
+       yi = y0
+       
+       # ¿Hay parámetros adicionales?
+       if param is None:
+           fun  = f
+       else:
+           fun = lambda a,b : f(a,b,param)
+        
+       
+       for i in range(len(x)):
+           #TODO: Recordar que los parámetros son adicionales
+           k1 = fun(yi,x[i])
+           k2 = fun(yi + 0.5*h*k1,x[i]+ 0.5*h)
+           k3 = fun(yi + 0.5*h*k2,x[i]+ 0.5*h)
+           k4 = fun(yi + h, x[i] + h)
+           yi = yi +(h/6.)*(k1 + 2.*k2 + 2.*k3 + k4)
+           res.append(yi)
+       return [x,array(res)]
+       
+    def denseOutput():
+       """
+       TODO: hay hacerlo :P
+       Permite enconctrar la solución en un punto arbitrario x
+       """
+       print("Dense output")
---
+++ testRK4.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 22:23:29 2018
+
+@author: Santiago
+"""
+
+from integrator import RK4
+import numpy as np
+import matplotlib.pyplot as plt
+
+def Ec(data,t0): #las distancias están en Mm = 10^6m
+    """    
+    data: arreglo con los datos [V_x, x, V_y, y]
+    t0: por compatibilidad.
+    """
+    G = 989.7#10^9m / (M_s day^2)
+    r = np.sqrt(data[1]**2 + data[3]**2)
+    k = -G/(r**3)
+                    #dv_x     , dx     , dv_y     , dy 
+    return np.array([k*data[1], data[0], k*data[3], data[2]])
+    
+y0 = np.array([0., 1.5*(10**2), 2.573, 0]) #[V_x, x, V_y, y]
+sol = RK4.solve(Ec, y0, 0., 365.*5,365.*5,None)
+t = sol[0]
+sol = sol[1]
+
+plt.plot(sol[:,1],sol[:,3])
+plt.show()
